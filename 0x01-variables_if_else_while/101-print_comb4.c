8230079ecbddb7ea64fcb60e983e80fde3bfe14e
Hamandawan <taonahama@gmail.com>
2023-02-18 04:27:34 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 102-print_comb5.c -o 102-print_comb5

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,7 +12,7 @@ int main(void)
 		int n, m, l;
 
 		for (n = 48; n < 58; n++)
-			{
+		{
 			for (m = 49; m < 58; m++)
 			{
 				for (l = 50; l < 58; l++)
@@ -26,22 +26,22 @@ int main(void)
 
 						putchar(l);
 
-		if (n != 55 || m != 56)
+						if (n != 55 || m != 56)
 
-		{
-						putchar(',');
-						putchar(' ');
+						{
+							putchar(',');
+							putchar(' ');
 						}
 
-						}
+					}
 
-						}
+				}
 
-						}
+			}
 
-						}
+		}
 
-				putchar('\n');
+		putchar('\n');
 
-				return (0);
-}
+		return (0);
+i}
