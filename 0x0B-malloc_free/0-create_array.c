139454dfb9de58b851947dbed89bda36dcac1195
Hamandawan <taonahama@gmail.com>
2023-03-13 12:48:07 -0700

    Update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,10 +16,10 @@ char *str;
 unsigned int i;
 
 str = malloc(sizeof(char) * size);
-if (size == 0 || str == NULL)
+	if (size == 0 || str == NULL)
 	return (NULL);
 	for (i = 0; i < size; i++)
-	str[i] = c;
+		str[i] = c;
 
 	return (str);
 
