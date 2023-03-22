94cefb2ac7f9c60a768832df9004bf0d91d6155b
Hamandawan <taonahama@gmail.com>
2023-03-22 11:50:26 -0700

    Update

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,25 +1,71 @@
-#ifndef _CALC_H_
-#define _CALC_H_
+#include "3-calc.h"
 
-#include <stdlib.h>
-#include <stdio.h>
 /**
- * struct op - Struct op
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * @op: The operator
- * @f: The function associated
+ * Return: add.
  */
-typedef struct op
+int op_add(int a, int b)
 {
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
+	return (a + b);
+}
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+/**
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: difference.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
-#endif
+/**
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: multiplication.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: division.
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: remainder of the division.
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
