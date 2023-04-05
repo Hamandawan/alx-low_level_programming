5f42749711c8b616949144422f43f5f4e5839862
Hamandawan <taonahama@gmail.com>
2023-04-05 10:03:55 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,47 +1,72 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index
- * of a linked list.
+ * free_listp2 - frees a linked list
  * @head: head of a list.
- * @index: index of the list where the node is
- * deleted.
  *
- * Return: 1 if it succeed, -1 if it fails
+ * Return: no return.
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+void free_listp2(listp_t **head)
 {
-	unsigned int i;
-	listint_t *prev;
-	listint_t *next;
+	listp_t *temp;
+	listp_t *curr;
 
-	prev = *head;
-
-	if (index != 0)
+	if (head != NULL)
 	{
-		for (i = 0; i < index - 1 && prev != NULL; i++)
+		curr = *head;
+		while ((temp = curr) != NULL)
 		{
-			prev = prev->next;
+			curr = curr->next;
+			free(temp);
 		}
+		*head = NULL;
 	}
+}
 
-	if (prev == NULL || (prev->next == NULL && index != 0))
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+	listint_t *curr;
+
+	hptr = NULL;
+	while (*h != NULL)
 	{
-		return (-1);
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (nnodes);
+			}
+		}
+
+		curr = *h;
+		*h = (*h)->next;
+		free(curr);
+		nnodes++;
 	}
 
-	next = prev->next;
-
-	if (index != 0)
-	{
-		prev->next = next->next;
-		free(next);
-	}
-	else
-	{
-		free(prev);
-		*head = next;
-	}
-
-	return (1);
+	*h = NULL;
+	free_listp2(&hptr);
+	return (nnodes);
 }
